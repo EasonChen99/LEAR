a457bc6521c0c33c980ddd6ea5c1f249b7b7c236
EasonChen99 <kuangyi.chen@tugraz.at>
2024-12-12 15:40:06 +0100

    add modified edge detector

--- core/backbone.py
+++ core/backbone.py
@@ -825,18 +825,188 @@ class Backbone_Edge(nn.Module):
 
 
 
+# class Encoder_Edge_Fusion(nn.Module):
+#     def __init__(self, input_dim=1, output_dim=128, norm_fn='batch', dropout=0.0):
+#         super(Encoder_Edge_Fusion, self).__init__()
+#         self.norm_fn = norm_fn
+#         if self.norm_fn == 'group':
+#             self.norm1 = nn.GroupNorm(num_groups=8, num_channels=64)
+#         elif self.norm_fn == 'batch':
+#             self.norm1 = nn.BatchNorm2d(64)
+#         elif self.norm_fn == 'instance':
+#             self.norm1 = nn.InstanceNorm2d(64)
+#         elif self.norm_fn == 'none':
+#             self.norm1 = nn.Sequential()
+
+#         self.flow_layer1 = nn.Conv2d(input_dim, 64, kernel_size=7, stride=2, padding=3)
+#         self.relu1 = nn.ReLU(inplace=True)
+#         self.in_planes = 64
+#         self.flow_layer2 = self._make_layer(64, stride=1)
+#         self.flow_layer3 = self._make_layer(96, stride=2)
+#         self.flow_layer4 = self._make_layer(128, stride=2)
+#         self.flow_layer5 = nn.Conv2d(128, output_dim, kernel_size=1)
+#         self.dropout = None
+#         if dropout > 0:
+#             self.dropout = nn.Dropout2d(p=dropout)
+        
+#         # edge encoder
+#         self.edge_layer1 = torch.nn.Sequential(
+#             torch.nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False),
+#             torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False)
+#         )
+#         self.edge_layer2 = torch.nn.Sequential(
+#             torch.nn.MaxPool2d(kernel_size=2, stride=2),
+#             torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False),
+#             torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False)
+#         )
+#         self.edge_layer3 = torch.nn.Sequential(
+#             torch.nn.MaxPool2d(kernel_size=2, stride=2),
+#             torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False),
+#             torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False),
+#             torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False)
+#         )
+#         self.edge_layer4 = torch.nn.Sequential(
+#             torch.nn.MaxPool2d(kernel_size=2, stride=2),
+#             torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False),
+#             torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False),
+#             torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False)
+#         )
+#         self.edge_layer5 = torch.nn.Sequential(
+#             torch.nn.MaxPool2d(kernel_size=2, stride=2),
+#             torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False),
+#             torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False),
+#             torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(inplace=False)
+#         )
+
+#         self.fusion1_flow = nn.Conv2d(192, 64, kernel_size=1)
+#         self.fusion1_edge = nn.Conv2d(192, 128, kernel_size=1)
+#         self.fusion2_flow = nn.Conv2d(352, 96, kernel_size=1)
+#         self.fusion2_edge = nn.Conv2d(352, 256, kernel_size=1)       
+#         self.fusion3_flow = nn.Conv2d(640, 128, kernel_size=1)
+#         self.fusion3_edge = nn.Conv2d(640, 512, kernel_size=1)     
+
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv2d):
+#                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+#             elif isinstance(m, (nn.BatchNorm2d, nn.InstanceNorm2d, nn.GroupNorm)):
+#                 if m.weight is not None:
+#                     nn.init.constant_(m.weight, 1)
+#                 if m.bias is not None:
+#                     nn.init.constant_(m.bias, 0)
+
+#     def _make_layer(self, dim, stride=1):
+#         layer1 = ResidualBlock(self.in_planes, dim, self.norm_fn, stride=stride)
+#         layer2 = ResidualBlock(dim, dim, self.norm_fn, stride=1)
+#         layers = (layer1, layer2)
+
+#         self.in_planes = dim
+#         return nn.Sequential(*layers)
+
+#     def forward(self, x):
+#         is_list = isinstance(x, tuple) or isinstance(x, list)
+#         if is_list:
+#             batch_dim = x[0].shape[0]
+#             x = torch.cat(x, dim=0)
+
+#         # layer1
+#         flow_feature_1 = self.relu1(self.norm1(self.flow_layer1(x)))
+#         x = (x + 1.) / 2. * 255.
+#         edge_feature_1 = self.edge_layer1(x)
+
+#         # layer2
+#         flow_feature_2 = self.flow_layer2(flow_feature_1)
+#         edge_feature_2 = self.edge_layer2(edge_feature_1)
+#         fusion_feature_1 = torch.cat((flow_feature_2, edge_feature_2), dim=1)
+#         flow_feature_2 = self.fusion1_flow(fusion_feature_1)
+#         edge_feature_2 = self.fusion1_edge(fusion_feature_1)
+
+#         # layer3
+#         flow_feature_3 = self.flow_layer3(flow_feature_2)
+#         edge_feature_3 = self.edge_layer3(edge_feature_2)
+#         fusion_feature_2 = torch.cat((flow_feature_3, edge_feature_3), dim=1)
+#         flow_feature_3 = self.fusion2_flow(fusion_feature_2)
+#         edge_feature_3 = self.fusion2_edge(fusion_feature_2)       
+
+#         # layer4
+#         flow_feature_4 = self.flow_layer4(flow_feature_3)
+#         edge_feature_4 = self.edge_layer4(edge_feature_3)
+#         fusion_feature_3 = torch.cat((flow_feature_4, edge_feature_4), dim=1)
+#         flow_feature_4 = self.fusion3_flow(fusion_feature_3)
+#         edge_feature_4 = self.fusion3_edge(fusion_feature_3)   
+
+#         # layer5
+#         flow_feature_5 = self.flow_layer5(flow_feature_4)
+#         edge_feature_5 = self.edge_layer5(edge_feature_4)
+
+#         if self.training and self.dropout is not None:
+#             flow_feature_5 = self.dropout(flow_feature_5)
+#         if is_list:
+#             flow_feature_5 = torch.split(flow_feature_5, [batch_dim, batch_dim], dim=0)
+
+#         return flow_feature_5, [x, edge_feature_1,edge_feature_2,edge_feature_3,edge_feature_4,edge_feature_5]
+
+# class Decoder_Edge(nn.Module):
+#     def __init__(self):
+#         super(Decoder_Edge, self).__init__()
+
+#         self.netScoreOne = torch.nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0)
+#         self.netScoreTwo = torch.nn.Conv2d(in_channels=128, out_channels=1, kernel_size=1, stride=1, padding=0)
+#         self.netScoreThr = torch.nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1, stride=1, padding=0)
+#         self.netScoreFou = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
+#         self.netScoreFiv = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
+
+#         self.netCombine = torch.nn.Sequential(
+#             torch.nn.Conv2d(in_channels=5, out_channels=1, kernel_size=1, stride=1, padding=0),
+#             torch.nn.Sigmoid()
+#         )
+
+#     def forward(self, tenVggList):
+#         tenInput, tenVggOne, tenVggTwo, tenVggThr, tenVggFou, tenVggFiv = tenVggList
+
+#         tenScoreOne = self.netScoreOne(tenVggOne)
+#         tenScoreTwo = self.netScoreTwo(tenVggTwo)
+#         tenScoreThr = self.netScoreThr(tenVggThr)
+#         tenScoreFou = self.netScoreFou(tenVggFou)
+#         tenScoreFiv = self.netScoreFiv(tenVggFiv)
+
+#         tenScoreOne = torch.nn.functional.interpolate(input=tenScoreOne, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+#         tenScoreTwo = torch.nn.functional.interpolate(input=tenScoreTwo, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+#         tenScoreThr = torch.nn.functional.interpolate(input=tenScoreThr, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+#         tenScoreFou = torch.nn.functional.interpolate(input=tenScoreFou, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+#         tenScoreFiv = torch.nn.functional.interpolate(input=tenScoreFiv, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+
+#         return self.netCombine(torch.cat([ tenScoreOne, tenScoreTwo, tenScoreThr, tenScoreFou, tenScoreFiv ], 1))
+
+# # rewrite edge detector for homogeneous achitecture
 class Encoder_Edge_Fusion(nn.Module):
     def __init__(self, input_dim=1, output_dim=128, norm_fn='batch', dropout=0.0):
         super(Encoder_Edge_Fusion, self).__init__()
         self.norm_fn = norm_fn
         if self.norm_fn == 'group':
             self.norm1 = nn.GroupNorm(num_groups=8, num_channels=64)
+            self.norm2 = nn.GroupNorm(num_groups=8, num_channels=64)
         elif self.norm_fn == 'batch':
             self.norm1 = nn.BatchNorm2d(64)
+            self.norm2 = nn.BatchNorm2d(64)
         elif self.norm_fn == 'instance':
             self.norm1 = nn.InstanceNorm2d(64)
+            self.norm2 = nn.InstanceNorm2d(64)
         elif self.norm_fn == 'none':
             self.norm1 = nn.Sequential()
+            self.norm2 = nn.Sequential()
 
         self.flow_layer1 = nn.Conv2d(input_dim, 64, kernel_size=7, stride=2, padding=3)
         self.relu1 = nn.ReLU(inplace=True)
@@ -850,53 +1020,24 @@ class Encoder_Edge_Fusion(nn.Module):
             self.dropout = nn.Dropout2d(p=dropout)
         
         # edge encoder
-        self.edge_layer1 = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False)
-        )
-        self.edge_layer2 = torch.nn.Sequential(
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-            torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False)
-        )
-        self.edge_layer3 = torch.nn.Sequential(
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-            torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False)
-        )
-        self.edge_layer4 = torch.nn.Sequential(
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-            torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False)
-        )
-        self.edge_layer5 = torch.nn.Sequential(
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-            torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False)
-        )
+        self.edge_layer1 = nn.Conv2d(input_dim, 64, kernel_size=7, stride=2, padding=3)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.in_planes = 64
+        self.edge_layer2 = self._make_layer(64, stride=1)
+        self.edge_layer3 = self._make_layer(96, stride=2)
+        self.edge_layer4 = self._make_layer(128, stride=2)
+        self.edge_layer5 = nn.Conv2d(128, output_dim, kernel_size=1)
 
-        self.fusion1_flow = nn.Conv2d(192, 64, kernel_size=1)
-        self.fusion1_edge = nn.Conv2d(192, 128, kernel_size=1)
-        self.fusion2_flow = nn.Conv2d(352, 96, kernel_size=1)
-        self.fusion2_edge = nn.Conv2d(352, 256, kernel_size=1)       
-        self.fusion3_flow = nn.Conv2d(640, 128, kernel_size=1)
-        self.fusion3_edge = nn.Conv2d(640, 512, kernel_size=1)     
+        self.fusion1_flow = nn.Conv2d(128, 64, kernel_size=1)
+        self.fusion1_edge = nn.Conv2d(128, 64, kernel_size=1)
+        self.fusion2_flow = nn.Conv2d(128, 64, kernel_size=1)
+        self.fusion2_edge = nn.Conv2d(128, 64, kernel_size=1)       
+        self.fusion3_flow = nn.Conv2d(192, 96, kernel_size=1)
+        self.fusion3_edge = nn.Conv2d(192, 96, kernel_size=1)   
+        self.fusion4_flow = nn.Conv2d(256, 128, kernel_size=1)
+        self.fusion4_edge = nn.Conv2d(256, 128, kernel_size=1)  
+        self.fusion5_flow = nn.Conv2d(256, 256, kernel_size=1)
+        self.fusion5_edge = nn.Conv2d(256, 256, kernel_size=1)    
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -924,32 +1065,38 @@ class Encoder_Edge_Fusion(nn.Module):
         # layer1
         flow_feature_1 = self.relu1(self.norm1(self.flow_layer1(x)))
         x = (x + 1.) / 2. * 255.
-        edge_feature_1 = self.edge_layer1(x)
+        edge_feature_1 = self.relu2(self.norm2(self.edge_layer1(x)))
+        fusion_feature_1 = torch.cat((flow_feature_1, edge_feature_1), dim=1)
+        flow_feature_1 = self.fusion1_flow(fusion_feature_1)
+        edge_feature_1 = self.fusion1_edge(fusion_feature_1)
 
         # layer2
         flow_feature_2 = self.flow_layer2(flow_feature_1)
         edge_feature_2 = self.edge_layer2(edge_feature_1)
-        fusion_feature_1 = torch.cat((flow_feature_2, edge_feature_2), dim=1)
-        flow_feature_2 = self.fusion1_flow(fusion_feature_1)
-        edge_feature_2 = self.fusion1_edge(fusion_feature_1)
+        fusion_feature_2 = torch.cat((flow_feature_2, edge_feature_2), dim=1)
+        flow_feature_2 = self.fusion2_flow(fusion_feature_2)
+        edge_feature_2 = self.fusion2_edge(fusion_feature_2)
 
         # layer3
         flow_feature_3 = self.flow_layer3(flow_feature_2)
         edge_feature_3 = self.edge_layer3(edge_feature_2)
-        fusion_feature_2 = torch.cat((flow_feature_3, edge_feature_3), dim=1)
-        flow_feature_3 = self.fusion2_flow(fusion_feature_2)
-        edge_feature_3 = self.fusion2_edge(fusion_feature_2)       
+        fusion_feature_3 = torch.cat((flow_feature_3, edge_feature_3), dim=1)
+        flow_feature_3 = self.fusion3_flow(fusion_feature_3)
+        edge_feature_3 = self.fusion3_edge(fusion_feature_3)       
 
         # layer4
         flow_feature_4 = self.flow_layer4(flow_feature_3)
         edge_feature_4 = self.edge_layer4(edge_feature_3)
-        fusion_feature_3 = torch.cat((flow_feature_4, edge_feature_4), dim=1)
-        flow_feature_4 = self.fusion3_flow(fusion_feature_3)
-        edge_feature_4 = self.fusion3_edge(fusion_feature_3)   
+        fusion_feature_4 = torch.cat((flow_feature_4, edge_feature_4), dim=1)
+        flow_feature_4 = self.fusion4_flow(fusion_feature_4)
+        edge_feature_4 = self.fusion4_edge(fusion_feature_4)   
 
         # layer5
         flow_feature_5 = self.flow_layer5(flow_feature_4)
         edge_feature_5 = self.edge_layer5(edge_feature_4)
+        fusion_feature_5 = torch.cat((flow_feature_4, edge_feature_4), dim=1)
+        flow_feature_5 = self.fusion5_flow(fusion_feature_5)
+        edge_feature_5 = self.fusion5_edge(fusion_feature_5)  
 
         if self.training and self.dropout is not None:
             flow_feature_5 = self.dropout(flow_feature_5)
@@ -963,10 +1110,10 @@ class Decoder_Edge(nn.Module):
         super(Decoder_Edge, self).__init__()
 
         self.netScoreOne = torch.nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0)
-        self.netScoreTwo = torch.nn.Conv2d(in_channels=128, out_channels=1, kernel_size=1, stride=1, padding=0)
-        self.netScoreThr = torch.nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1, stride=1, padding=0)
-        self.netScoreFou = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
-        self.netScoreFiv = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
+        self.netScoreTwo = torch.nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0)
+        self.netScoreThr = torch.nn.Conv2d(in_channels=96, out_channels=1, kernel_size=1, stride=1, padding=0)
+        self.netScoreFou = torch.nn.Conv2d(in_channels=128, out_channels=1, kernel_size=1, stride=1, padding=0)
+        self.netScoreFiv = torch.nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1, stride=1, padding=0)
 
         self.netCombine = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels=5, out_channels=1, kernel_size=1, stride=1, padding=0),
@@ -988,7 +1135,8 @@ class Decoder_Edge(nn.Module):
         tenScoreFou = torch.nn.functional.interpolate(input=tenScoreFou, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
         tenScoreFiv = torch.nn.functional.interpolate(input=tenScoreFiv, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
 
-        return self.netCombine(torch.cat([ tenScoreOne, tenScoreTwo, tenScoreThr, tenScoreFou, tenScoreFiv ], 1))    
+        return self.netCombine(torch.cat([ tenScoreOne, tenScoreTwo, tenScoreThr, tenScoreFou, tenScoreFiv ], 1))
+
 
 class Backbone_Edge_FF(nn.Module):
     def __init__(self, args):
