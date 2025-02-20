ac3e5db80338bfab1101b1c4055835454246b987
EasonChen99 <kuangyi.chen@tugraz.at>
2025-02-20 16:50:27 +0100

    update

--- core/datasets_m3ed.py
+++ core/datasets_m3ed.py
@@ -20,10 +20,16 @@ def get_calib_m3ed(sequence, camera):
             return torch.tensor([1034.39696079, 1034.73607278, 636.27410756, 364.73952748])
         elif sequence in ['falcon_outdoor_day_penno_parking_1', 'falcon_outdoor_day_penno_parking_2']:
             return torch.tensor([1033.22781771, 1032.05548869, 631.84536312, 360.7175681])
-        elif sequence in ['spot_outdoor_day_srt_under_bridge_1', 'spot_outdoor_day_srt_under_bridge_1']:
-            return torch.tensor([1031.52186613, 1031.70276624,  633.53886647,  364.34446137])
+        # elif sequence in ['falcon_outdoor_night_penno_parking_1', 'falcon_outdoor_night_penno_parking_2']:
+        #     return torch.tensor([1034.39696079, 1034.73607278,  636.27410756,  364.73952748])
+        # elif sequence in ['spot_outdoor_day_srt_under_bridge_1', 'spot_outdoor_day_srt_under_bridge_1']:
+        #     return torch.tensor([1031.52186613, 1031.70276624,  633.53886647,  364.34446137])
         elif sequence in ['spot_forest_road_1', 'spot_forest_road_3']:
             return torch.tensor([1032.96138833, 1032.8263147,   632.38290823, 368.57212974])
+        elif sequence in ['car_urban_day_penno_big_loop', 'car_urban_day_penno_small_loop']:
+            return torch.tensor([1031.88399229, 1031.48192315 , 634.29808475 , 366.39105342])
+        # elif sequence in ['car_urban_night_penno_big_loop', 'car_urban_night_penno_small_loop']:
+        #     return torch.tensor([1031.84262859, 1030.10381777 , 635.71409589 , 365.59991372])
         else:
             raise TypeError("Sequence Not Available")
     else:
@@ -33,10 +39,16 @@ def get_calib_m3ed(sequence, camera):
             return torch.tensor([1034.61302587, 1034.83604567,  638.12992827,  366.88002829])
         elif sequence in ['falcon_outdoor_day_penno_parking_1', 'falcon_outdoor_day_penno_parking_2']:
             return torch.tensor([1033.22781771, 1032.05548869, 631.84536312, 360.7175681])
-        elif sequence in ['spot_outdoor_day_srt_under_bridge_1', 'spot_outdoor_day_srt_under_bridge_1']:
-            return torch.tensor([1030.29161359, 1030.9024083, 634.79835424, 368.11576903])
+        # elif sequence in ['falcon_outdoor_night_penno_parking_1', 'falcon_outdoor_night_penno_parking_2']:
+        #     return torch.tensor([1034.61302587, 1034.83604567,  638.12992827,  366.88002829])
+        # elif sequence in ['spot_outdoor_day_srt_under_bridge_1', 'spot_outdoor_day_srt_under_bridge_1']:
+        #     return torch.tensor([1030.29161359, 1030.9024083, 634.79835424, 368.11576903])
         elif sequence in ['spot_forest_road_1', 'spot_forest_road_3']:
             return torch.tensor([1032.26867081, 1032.57448492,  632.85489859,  370.0490076 ])
+        elif sequence in ['car_urban_day_penno_big_loop', 'car_urban_day_penno_small_loop']:
+            return torch.tensor([1031.36879978 ,1031.06491961 , 634.87768084 , 367.62546105])
+        # elif sequence in ['car_urban_night_penno_big_loop', 'car_urban_night_penno_small_loop']:
+        #     return torch.tensor([1030.46186128, 1029.51180204,  635.69022466 , 364.32444857])
         else:
             raise TypeError("Sequence Not Available")
 
@@ -56,16 +68,31 @@ def get_left_right_T(sequence):
                              [ 3.8672e-04,  1.0000e+00, -9.8024e-04,  9.3651e-04],
                              [ 3.2097e-03,  9.7899e-04,  9.9999e-01,  3.4862e-04],
                              [ 0.0000e+00,  0.0000e+00,  0.0000e+00,  1.0000e+00]])
-    elif sequence in ['spot_outdoor_day_srt_under_bridge_1', 'spot_outdoor_day_srt_under_bridge_1']:
-        return torch.tensor([[ 9.9999e-01, -6.4771e-04, -3.6507e-03, -1.2011e-01],
-                             [ 6.3675e-04,  1.0000e+00, -3.0024e-03,  8.7946e-04],
-                             [ 3.6526e-03,  3.0000e-03,  9.9999e-01, -2.8266e-04],
-                             [ 0.0000e+00,  0.0000e+00,  0.0000e+00,  1.0000e+00]])
+    # elif sequence in ['falcon_outdoor_night_penno_parking_1', 'falcon_outdoor_night_penno_parking_2']:
+    #     return torch.tensor([[ 9.99993617e-01, -6.66130268e-04, -3.51029598e-03, -1.20179395e-01],
+    #                          [ 6.66610390e-04,  9.99999769e-01,  1.35607254e-04,  9.10326972e-04],
+    #                          [ 3.51020483e-03, -1.37946389e-04,  9.99993830e-01, -4.30589681e-04],
+    #                          [ 0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  1.00000000e+00]])        
+    # elif sequence in ['spot_outdoor_day_srt_under_bridge_1', 'spot_outdoor_day_srt_under_bridge_1']:
+    #     return torch.tensor([[ 9.9999e-01, -6.4771e-04, -3.6507e-03, -1.2011e-01],
+    #                          [ 6.3675e-04,  1.0000e+00, -3.0024e-03,  8.7946e-04],
+    #                          [ 3.6526e-03,  3.0000e-03,  9.9999e-01, -2.8266e-04],
+    #                          [ 0.0000e+00,  0.0000e+00,  0.0000e+00,  1.0000e+00]])
     elif sequence in ['spot_forest_road_1', 'spot_forest_road_3']:
         return torch.tensor([[ 9.99992821e-01, -7.03218854e-04, -3.72345760e-03, -1.20137685e-01],
                              [ 6.95582202e-04,  9.99997653e-01, -2.05185517e-03,  1.03034216e-03],
                              [ 3.72489176e-03,  2.04925047e-03, 9.99990963e-01 , 4.66687603e-04],
                              [ 0.00000000e+00 , 0.00000000e+00 , 0.00000000e+00  ,1.00000000e+00]])
+    elif sequence in ['car_urban_day_penno_big_loop', 'car_urban_day_penno_small_loop']:
+        return torch.tensor([[ 9.99993926e-01 ,-7.53276578e-04, -3.40299857e-03, -1.20220978e-01],
+                             [ 7.47230114e-04,  9.99998141e-01 ,-1.77772679e-03 , 9.05842201e-04],
+                             [ 3.40433136e-03 , 1.77517317e-03,  9.99992630e-01, -1.13660539e-04],
+                             [ 0.00000000e+00 , 0.00000000e+00 , 0.00000000e+00 , 1.00000000e+00]])
+    # elif sequence in ['car_urban_night_penno_big_loop', 'car_urban_night_penno_small_loop']:
+    #     return torch.tensor([[ 9.99996013e-01 ,-6.08160070e-04, -2.75745341e-03, -1.20188402e-01],
+    #                          [ 6.09451349e-04,  9.99999705e-01,  4.67470872e-04 , 1.06054268e-03],
+    #                          [ 2.75716830e-03, -4.69149542e-04,  9.99996089e-01,  1.07289756e-04],
+    #                          [ 0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  1.00000000e+00]])        
     else:
         raise TypeError("Sequence Not Available")
 
@@ -87,12 +114,10 @@ class DatasetM3ED(Dataset):
                     #   'falcon_indoor_flight_1', 
                     #   'falcon_indoor_flight_2', 
                     #   'falcon_indoor_flight_3', 
-                      'falcon_outdoor_day_penno_parking_1',
-                      'falcon_outdoor_day_penno_parking_2',
-                    #   'falcon_outdoor_night_penno_parking_1',
-                    #   'falcon_outdoor_night_penno_parking_2',
-                    # 'spot_outdoor_day_srt_under_bridge_1',
-                    # 'spot_outdoor_day_srt_under_bridge_2'
+                    #   'falcon_outdoor_day_penno_parking_1',
+                    #   'falcon_outdoor_day_penno_parking_2',
+                    'car_urban_day_penno_big_loop',
+                    'car_urban_day_penno_small_loop',
                      ]
         
         for dir in scene_list:
@@ -103,10 +128,23 @@ class DatasetM3ED(Dataset):
             poses = h5py.File(pose_path,'r')
             Ln_T_L0 = poses['Ln_T_L0'] 
 
+            if dir in ['falcon_indoor_flight_1', 'falcon_indoor_flight_2', 'falcon_indoor_flight_3']:
+                left = 70
+                right = 70
+            elif dir in ['falcon_outdoor_day_penno_parking_1', 'falcon_outdoor_day_penno_parking_2']:
+                left = 100
+                right = 100
+            elif dir in ['car_urban_day_penno_big_loop']:
+                left = 90
+                right = 48
+            elif dir in ['car_urban_day_penno_small_loop']:
+                left = 70
+                right = 20
+            else:
+                raise "Sequence doesn't exist in the list"
+
             for idx in range(Ln_T_L0.shape[0]):
-                # if idx + 70 < Ln_T_L0.shape[0] and idx > 70:    # falcon_indoor_flight_1 2 3
-                if idx + 100 < Ln_T_L0.shape[0] and idx > 100:  # falcon_outdoor_day_penno_parking 1 2
-                # if idx + 140 < Ln_T_L0.shape[0] and idx > 140:  # spot_outdoor
+                if idx + right < Ln_T_L0.shape[0] and idx > left: 
                     if not os.path.exists(os.path.join(self.root_dir, dir, "local_maps", f"point_cloud_{idx:05d}"+'.h5')):
                         continue
                     if not os.path.exists(os.path.join(self.root_dir, dir, f"event_frames_{self.event_representation}", 'left', f"event_frame_{idx:05d}"+'.npy')):
